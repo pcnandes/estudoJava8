57254c82350f78e785988230bd3e7ab88a8006cf
Paulo <paulo.fernandes@serpro.gov.br>
2020-01-02 07:39:57 -0300

    Aula 3

--- java8_alura/novos_recursos/src/java8/AlunoServico.java
+++ java8_alura/novos_recursos/src/java8/AlunoServico.java
@@ -1,6 +1,7 @@
 package java8;
 
 import java.util.List;
+import java.util.Optional;
 
 public class AlunoServico {
 	Aluno joao = new Aluno("João", 1L);
@@ -10,5 +11,11 @@ public class AlunoServico {
 	public List<Aluno> listar() {
 		// return Collections.unmodifiableList(Arrays.asList(joao, fernanda));
 		return List.of(joao, fernanda, nulo);
+	}
+	
+	public Optional<Aluno> listarPorCpf(Long cpf) {
+		return listar().stream()
+				.filter(a -> a.getCpf().equals(cpf))
+				.findAny();
 	}
 }
--- java8_alura/novos_recursos/src/java8/Principal.java
+++ java8_alura/novos_recursos/src/java8/Principal.java
@@ -1,20 +1,48 @@
 package java8;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		AlunoServico alunoServico = new AlunoServico();
+		// AlunoServico alunoServico = new AlunoServico();
+		var alunoServico = new AlunoServico();
 		
 		alunoServico.listar()
 			.stream()
 			.forEach(System.out::println);
 		
-		List<String> nomes = transformaEmListaDeNomes(alunoServico.listar());
+		// List<String> nomes = transformaEmListaDeNomes(alunoServico.listar());
+		var nomes = transformaEmListaDeNomes(alunoServico.listar());
 		System.out.println(nomes);
+		
+		
+		// Optional<Aluno> aluno = alunoServico.listarPorCpf(1L);
+		var aluno = alunoServico.listarPorCpf(1L);
+		/* ANTIGO Optioal
+		if(aluno.isPresent()) {
+			System.out.println("Aluno encontrado: " + aluno.get());
+		} else {
+			System.out.println("CPF não encontrado!");
+		}*/
+		
+		// NOVO Optioal
+		aluno.ifPresentOrElse(System.out::println,
+				() -> System.out.println("CPF não encontrado!"));
+		
+		
+		// encadeando optionals -> or -> se for null ele executa uma nova função. Caso encontre os proximos serão ignorados
+		// Optional<Aluno> alunoRecuperado = alunoServico.listarPorCpf(0L)
+		var alunoRecuperado = alunoServico.listarPorCpf(0L)
+				.or(() -> alunoServico.listarPorCpf(11111L))
+				.or(() -> alunoServico.listarPorCpf(1L));
+		
+		System.out.println("Aluno recuperado " + alunoRecuperado.get());
+		
+		
 	}
 	
 	public static List<String> transformaEmListaDeNomes(List<Aluno> lista) {
